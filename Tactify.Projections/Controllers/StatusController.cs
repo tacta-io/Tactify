1f6e41fc109fe90bc94b1a11568a148f8b4673d7
nusretasinanovic <nusreta.sinanovic@tacta.io>
2023-12-28 16:18:16 +0100

    projections monitoring cleanup

--- Tactify.Projections/Controllers/StatusController.cs
+++ Tactify.Projections/Controllers/StatusController.cs
@@ -1,7 +1,5 @@
-﻿using System.Text;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Tacta.EventStore.Projector;
-using Tacta.EventStore.Repository;
 
 namespace Tactify.Projections.Controllers
 {
@@ -10,37 +8,18 @@ namespace Tactify.Projections.Controllers
     public class StatusController : ControllerBase
     {
         private readonly IProjectionProcessor? _projectionProcessor;
-        private readonly IEventStoreRepository _eventStoreRepository;
 
-        public StatusController(IServiceProvider serviceProvider, IEventStoreRepository eventStoreRepository)
+        public StatusController(IServiceProvider serviceProvider)
         {
             _projectionProcessor = serviceProvider.GetService(typeof(IProjectionProcessor)) as IProjectionProcessor;
-            _eventStoreRepository = eventStoreRepository;
         }
 
         [HttpGet]
-        public ContentResult Get()
+        public async Task<ContentResult> Get()
         {
             if (_projectionProcessor == null) return base.Content("No data", "text/html");
 
-            var sequence = _eventStoreRepository.GetLatestSequence().ConfigureAwait(false).GetAwaiter().GetResult();
-
-            var statuses = _projectionProcessor.Status();
-
-            var data = new StringBuilder();
-
-            foreach (var status in statuses)
-            {
-                data.Append($"{{projection:'{status.Key}', sequence:'{status.Value}'}},");
-            }
-
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "Resources/status.html");
-            var html = System.IO.File.ReadAllText(path);
-
-            var content = new StringBuilder(html)
-                .Replace("{sequence}", sequence.ToString())
-                .Replace("{data}", data.ToString())
-                .ToString();            
+            var content = await _projectionProcessor.Status("Tactify").ConfigureAwait(false);
 
             return base.Content(content, "text/html");
         }
